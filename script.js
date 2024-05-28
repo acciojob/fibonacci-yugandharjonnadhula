7c4ea059c8aae7ef3fe226e6642f3642623c2391
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-28 17:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+// your code here\
+	 let a = 0, b = 1, next;
+
+    // Iteratively calculate the Fibonacci number
+    for (let i = 2; i <= num; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 }
 
-module.exports = fibonacci;
+// module.exports = fibonacci;
