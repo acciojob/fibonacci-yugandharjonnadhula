b60529032f6063626b5298745659ff54a409568b
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-29 22:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function fibonacci(num) {
-// your code here\
-	 let a = 0, b = 1, next;
+    if (num < 0) {
+        throw new Error("Input should be a non-negative integer");
+    }
+
+    // Base cases
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0, b = 1;
 
     // Iteratively calculate the Fibonacci number
     for (let i = 2; i <= num; i++) {
-        next = a + b;
+        const next = a + b;
         a = b;
         b = next;
     }
 
-    return b;
+    return a; // a will be the nth Fibonacci number after the loop
 }
 
-// module.exports = fibonacci;
+// Example usage:
+console.log(fibonacci(num));
